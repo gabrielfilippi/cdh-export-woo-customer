51dc847df676c07290fcc73e30a4d21757966004
gabrielfilippi <gabriel._gf@hotmail.com>
2025-01-16 10:39:35 -0300

    separate js to php code and add comments

---
+++ assets/js/cdh-export-woo-customer.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+    $('#start-export').on('click', function() {
+        const button = $(this);
+        button.prop('disabled', true).text('Exportando...');
+        $('#export-status').text('');
+
+        const nonce = '<?php echo wp_create_nonce( "woocommerce_admin" ); ?>';
+        const processExport = (offset = 0) => {
+            $.post(ajaxurl, {
+                action: 'wc_hpos_start_export',
+                _wpnonce: nonce, // Inclua o nonce aqui
+                offset: offset
+            }, function(response) {
+                if (response.success) {
+                    $('#export-status').text(response.data.message);
+                    if (response.data.next_offset) {
+                        processExport(response.data.next_offset);
+                    } else {
+                        button.prop('disabled', false).text('Iniciar Exportação');
+                        $('#export-status').append('<br>Exportação concluída! <a href="' + response.data.file_url + '" target="_blank">Baixar CSV</a>');
+                    }
+                } else {
+                    $('#export-status').text('Erro: ' + response.data);
+                    button.prop('disabled', false).text('Iniciar Exportação');
+                }
+            });
+        };
+
+        processExport();
+    });
+});
--- cdh-export-woo-customer.php
+++ cdh-export-woo-customer.php
@@ -5,76 +5,62 @@
  * Plugin URI: https://github.com/gabrielfilippi/cdh-export-woo-customer
  * Author: CodeHive
  * Author URI: https://codehive.com.br
- * Version: 1.0.0
+ * Version: 1.1.0
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: chd_image_converter
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Saia se acessado diretamente
+    exit; // security if access directly
 }
 
 class WC_HPOS_Export_To_CSV_Optimized {
-    private $batch_size = 100; // Número de pedidos processados por lote
+    private $batch_size = 100; // batch size to proccess orders
 
     public function __construct() {
         add_action( 'admin_menu', [ $this, 'add_export_menu' ] );
         add_action( 'wp_ajax_wc_hpos_start_export', [ $this, 'start_export_process' ] );
     }
 
+    /**
+     * Add submenu inside Woocommerce Menu
+     */
     public function add_export_menu() {
         add_submenu_page(
             'woocommerce',
-            'Exportar Clientes para CSV',
-            'Exportar CSV',
+            'Exportar Clientes para Excel',
+            'Exportar Clientes',
             'manage_woocommerce',
             'wc-hpos-export-csv',
             [ $this, 'render_export_page' ]
         );
     }
 
+    /**
+     * Render Export Page in ADMIN
+     */
     public function render_export_page() {
+        // register js file
+        wp_enqueue_script(
+            'wc-export-csv',
+            plugins_url('js/cdh-export-woo-customer.min.js', __FILE__),
+            [ 'jquery' ],
+            '1.0.0',
+            true // load on footer
+        );
+
+        // localize js file
+        wp_localize_script('wc-export-csv', 'exportCsvParams', [
+            'nonce' => wp_create_nonce('woocommerce_admin'),
+        ]);
+    
+        //print HTML
         echo '<div class="wrap">';
-        echo '<h1>Exportar Dados de Clientes que Já Compraram para CSV</h1>';
-        echo '<button id="start-export" class="button button-primary">Iniciar Exportação</button>';
-        echo '<p id="export-status"></p>';
+            echo '<h1>Exportar Dados de Clientes que Já Compraram para Excel</h1>';
+            echo '<button id="start-export" class="button button-primary">Iniciar Exportação</button>';
+            echo '<p id="export-status"></p>';
         echo '</div>';
-        ?>
-        <script type="text/javascript">
-            jQuery(document).ready(function($) {
-                $('#start-export').on('click', function() {
-                    const button = $(this);
-                    button.prop('disabled', true).text('Exportando...');
-                    $('#export-status').text('');
-
-                    const nonce = '<?php echo wp_create_nonce( "woocommerce_admin" ); ?>';
-                    const processExport = (offset = 0) => {
-                        $.post(ajaxurl, {
-                            action: 'wc_hpos_start_export',
-                            _wpnonce: nonce, // Inclua o nonce aqui
-                            offset: offset
-                        }, function(response) {
-                            if (response.success) {
-                                $('#export-status').text(response.data.message);
-                                if (response.data.next_offset) {
-                                    processExport(response.data.next_offset);
-                                } else {
-                                    button.prop('disabled', false).text('Iniciar Exportação');
-                                    $('#export-status').append('<br>Exportação concluída! <a href="' + response.data.file_url + '" target="_blank">Baixar CSV</a>');
-                                }
-                            } else {
-                                $('#export-status').text('Erro: ' + response.data);
-                                button.prop('disabled', false).text('Iniciar Exportação');
-                            }
-                        });
-                    };
-
-                    processExport();
-                });
-            });
-        </script>
-        <?php
     }
 
     public function start_export_process() {
@@ -84,38 +70,39 @@ class WC_HPOS_Export_To_CSV_Optimized {
         $upload_dir = wp_upload_dir();
         $file_path = $upload_dir['basedir'] . '/codehive_export_woo_customer_and_users.csv';
 
-        // Se for o primeiro lote, cria o arquivo e adiciona o cabeçalho
+        // If it is the first batch, create the file and add the header
         if ( $offset === 0 && file_exists( $file_path ) ) {
             unlink( $file_path ); // Remove arquivo antigo
         }
 
-        // Abre o arquivo e adiciona o BOM no início (para suportar acentuação em ferramentas como Excel)
+        // Open the file and add the BOM at the beginning (to support accentuation in tools like Excel)
         $file = fopen( $file_path, $offset === 0 ? 'w' : 'a' );
         if ( $offset === 0 ) {
-            // Adiciona o BOM para suportar UTF-8
+            // Add BOM to support UTF-8
             fprintf($file, chr(0xEF).chr(0xBB).chr(0xBF));
 
-            // Escreve o cabeçalho separado por ";"
+            // Write the header separated by ";"
             $header = array_map(fn($col) => mb_convert_encoding($col, 'UTF-8'), [ 'ID', 'Nome', 'Sobrenome', 'Nome Completo', 'Telefone' ]);
             fputcsv( $file, $header, ';', '"' );
         }
 
-        // Recupera pedidos em lotes
+        // Retrieve orders in batches
         $orders = wc_get_orders( [
             'limit'  => $this->batch_size,
             'offset' => $offset,
         ] );
 
         foreach ( $orders as $order ) {
-            // Certifique-se de que o objeto é uma instância de WC_Order e não um reembolso
+            // Make sure the object is an instance of WC_Order and not a refund
             if ( ! $order instanceof WC_Order || $order instanceof WC_Order_Refund ) {
-                continue; // Ignora reembolsos ou objetos inválidos
+                continue; // Ignore refunds or invalid objects
             }
 
+            //If you are invited, we generate a UUID for you
             $user_id = $order->get_user_id();
-
             $user_id = $user_id ? $user_id : wp_generate_uuid4();
 
+            //get data from order
             $first_name = $order->get_billing_first_name();
             $last_name  = $order->get_billing_last_name();
             $full_name  = $first_name . ' ' . $last_name;
@@ -131,12 +118,12 @@ class WC_HPOS_Export_To_CSV_Optimized {
             fputcsv( $file, $row, ';', '"' );
         }
 
-         // Verifica se há mais pedidos para processar
+        // Check if there are more requests to process
         $next_offset = count( $orders ) === $this->batch_size ? $offset + $this->batch_size : null;
 
-        // Se for o último lote, pula uma linha e executa a query para usuários
+        // If it is the last batch, skip a line and execute the query for users (to get user that never bought)
         if ( !$next_offset ) {
-            fputcsv( $file, [], ';' );  // Pula uma linha
+            fputcsv( $file, [], ';' );  // Skip a line
 
             global $wpdb;
             $sql = "
@@ -153,7 +140,7 @@ class WC_HPOS_Export_To_CSV_Optimized {
             
             $results = $wpdb->get_results( $sql );
 
-            // Adiciona os dados da query ao CSV
+            // Add the query data to the CSV
             foreach ( $results as $user ) {
                 $row = [
                     mb_convert_encoding($user->id, 'UTF-8'),
